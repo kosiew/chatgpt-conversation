8323f5c290211dc8e84bb5a49c6a94f15591d1c4
Siew Kam Onn <kosiew@gmail.com>
2024-08-02 10:39:22 +0800

    added pythoninfo.txt

--- activate_venv.sh
+++ activate_venv.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 # Script to activate Python virtual environment
-VENV_PATH="/Users/kosiew/.venv/chatgpt_conversation" activate_venv
+VENV_PATH="$VENV_ROOT/chatgpt_conversation" activate_venv
 
