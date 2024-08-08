03a497c9b12b0757bd4c3021c6d702d24e5f0daf
Siew Kam Onn <kosiew@gmail.com>
2024-08-08 15:05:29 +0800

    install pandas matplotlib numpy for multiagent

---
+++ src/multi_agent.py
@@ -0,0 +1,10 @@
+import json
+from io import StringIO
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from IPython.display import Image
+from openai import OpenAI
+
+client = OpenAI()
